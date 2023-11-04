7c510e49d826da00750aa5355aa23c4cf7067dc0
Paweł <neotheprogramist@proton.me>
2023-11-04 11:55:54 +0100

    example usage of DbController in DcClient

---
+++ src/client.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from db import DatabaseController
+
+
+class Client(commands.Bot):
+    def __init__(self, controller: DatabaseController):
+        super().__init__(command_prefix="/", intents=discord.Intents().all())
+        self.cogslist = ["init", "start", "stop", "cancel", "join"]
+
+        self.controller = controller
+
+    async def on_ready(self):
+        print(" Logged in as " + self.user.name)
+        synced = await self.tree.sync()
+        print(" Slash CMDs Synced " + str(len(synced)) + " Commands")
+        fetched_strategy = await self.example_usage()
+        print(f"fetched strategy: {fetched_strategy}")
+
+    async def setup_hook(self):
+        for ext in self.cogslist:
+            await self.load_extension("cogs." + ext)
+
+    async def example_usage(self):
+        return self.controller.get_strategy_by_discord_id(1)
--- src/main.py
+++ src/main.py
@@ -1,15 +1,12 @@
-import asyncio
+import os
 from db import DatabaseController, Strategy, StrategyStatus
 from uuid import uuid4
 from datetime import datetime
-import discord
-from discord.ext import commands
-import os
+from client import Client
 from dotenv import load_dotenv
 
-TOKEN = os.getenv("DISCORD_TOKEN")
 
-async def main():
+def main():
     controller = DatabaseController()
 
     example_strategy = Strategy(
@@ -18,27 +15,14 @@ async def main():
     controller.add_strategy(example_strategy)
 
     fetched_strategy = controller.get_strategy_by_discord_id(1)
+
+    client = Client(controller)
+
     print(fetched_strategy)
 
-class Client(commands.Bot):
-  def __init__(self):
-    super().__init__(command_prefix='/', intents=discord.Intents().all())
-    self.cogslist = ["init", "start", "stop", "cancel", "join"]
     load_dotenv()
+    client.run(os.getenv("DISCORD_TOKEN"))
 
-  async def on_ready(self):
-    print(" Logged in as " + self.user.name)
-    synced = await self.tree.sync()
-    print(" Slash CMDs Synced "+ str(len(synced)) + " Commands")
-
-  async def setup_hook(self):
-    for ext in self.cogslist:
-      await self.load_extension("cogs."+ext)
-
-client = Client()
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    client.run(TOKEN)
-    
+    main()
